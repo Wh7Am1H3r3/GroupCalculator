741212cc7ff6d1d28e1bf111d1e2c811c9b86b98
APCSP <apcsp@Cart-Mac-4.local>
2024-09-27 11:24:13 -0700

    Added menu for calculator

---
+++ calculator.swift
@@ -0,0 +1,47 @@
+
+// Menu function to display options to user
+func showMenu() {
+    print("""
+    please select an operation:
+    1: Add
+    2: Subtract
+    3: Multiply
+    4: Divide
+    5: Exponent
+    6: Remainder
+    """)
+}
+
+
+print("please enter your two numbers.")
+let number1 = readLine()
+let number2 = readLine()
+
+
+
+while true{
+showMenu()
+let menuSelection = readLine()
+if menuSelection == "1" {
+    
+}
+if menuSelection == "2" {
+    
+}
+if menuSelection == "3" {
+    
+}
+if menuSelection == "4" {
+   
+}
+if menuSelection == "5" {
+    
+}
+if menuSelection == "6" {
+
+}
+else {
+    print("Not a valid operation. Please select an operation using numbers 1-6.")
+}
+}
+
